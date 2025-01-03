1befe860723b56280a24aee9d75255b1ed8d8ed6
Owen Leung <owenleung2005@gmail.com>
2025-01-02 23:24:31 -0500

    push

---
+++ frontend/src/pages/CreatePost.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import axios from "axios";
+import { Formik, Form, Field, ErrorMessage} from 'formik';
+import * as Yup from 'yup'
+
+function CreatePost() {
+    const initialValues = {
+        title: "",
+        postText: "",
+        username: ""
+    };
+
+    // validates form inputs
+    const validationSchema = Yup.object().shape({
+        title: Yup.string().required("You msut intput titel"),
+        postText: Yup.string().required(),
+        username:Yup.string().min(3).max(15).required()
+    });
+
+    const onSubmit = (data) => {
+        axios.post("http://localhost:3001/posts", data).then((response) => {
+            console.log("IT WORKED");
+          });
+    };
+
+    return (
+        <div className="createPostPage"> 
+            <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}> 
+                <Form className="formContainer">
+                <label>Title: </label>
+                <ErrorMessage name="title" component="span" />
+                    <Field 
+                        id="inputCreatePost" 
+                        name="title" 
+                        placeholder="(Ex. Title)"
+                    />
+                    <label>Post: </label>
+                    <ErrorMessage name="postText" component="span" />
+                    <Field 
+                        id="inputCreatePost" 
+                        name="postText" 
+                        placeholder="(Ex. Post)"
+                    />
+                    <label>Username: </label>
+                    <ErrorMessage name="username" component="span" />
+                    <Field 
+                        id="inputCreatePost" 
+                        name="username" 
+                        placeholder="(Ex. John123)"
+                    />
+                    <button type="submit"> Create Post </button>
+                </Form>
+            </Formik>
+        </div>
+    )
+}
+
+export default CreatePost
