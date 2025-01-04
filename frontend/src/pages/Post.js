05fcb976ca1cf9d5f966146560f94a201b8391be
Owen Leung <owenleung2005@gmail.com>
2025-01-04 11:38:57 -0500

    9

--- frontend/src/pages/Post.js
+++ frontend/src/pages/Post.js
@@ -16,18 +16,30 @@ function Post() {
     axios.get(`http://localhost:3001/comments/${id}`).then((response) => {
       setComments(response.data);
     });
-  }, []);
+  }, [id]);
 
   const addComment = () => {
     axios
-      .post("http://localhost:3001/comments", {
-        commentBody: newComment,
-        PostId: id,
-      })
+      .post(
+        "http://localhost:3001/comments",
+        {
+          commentBody: newComment,
+          PostId: id,
+        },
+        {
+          headers: {
+            accessToken: sessionStorage.getItem("accessToken"),
+          },
+        }
+      )
       .then((response) => {
-        const commentToAdd = { commentBody: newComment };
-        setComments([...comments, commentToAdd]);
-        setNewComment("");
+        if (response.data.error) {
+          console.log(response.data.error)
+        } else {
+          const commentToAdd = { commentBody: newComment };
+          setComments([...comments, commentToAdd]);
+          setNewComment("");
+        }
       });
   };
 
--- frontend/src/pages/Registration.js
+++ frontend/src/pages/Registration.js
@@ -2,7 +2,6 @@ import React from 'react'
 import axios from "axios";
 import { Formik, Form, Field, ErrorMessage} from 'formik';
 import * as Yup from 'yup'
-import { useNavigate } from 'react-router-dom';
 
 function Registration() {
     const initialValues = {
@@ -24,28 +23,36 @@ function Registration() {
 
   return (
     <div>
-        <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}> 
-            <Form className="formContainer">
-                <label>Username: </label>
-                <ErrorMessage name="username" component="span" />
-                <Field 
-                    id="inputCreatePost" 
-                    name="username" 
-                    placeholder="(Ex. John123)"
-                />
-                <label>Password: </label>
-                <ErrorMessage name="username" component="span" />
-                <Field 
-                    type="password"
-                    id="inputCreatePost" 
-                    name="password" 
-                    placeholder="(Your Password...)"
-                />
-            <button type="submit"> Register </button>
-            </Form>
-        </Formik>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        <Form className="formContainer">
+          <label>Username: </label>
+          <ErrorMessage name="username" component="span" />
+          <Field
+            autocomplete="off"
+            id="inputCreatePost"
+            name="username"
+            placeholder="(Ex. John123...)"
+          />
+
+          <label>Password: </label>
+          <ErrorMessage name="password" component="span" />
+          <Field
+            autocomplete="off"
+            type="password"
+            id="inputCreatePost"
+            name="password"
+            placeholder="Your Password..."
+          />
+
+          <button type="submit"> Register</button>
+        </Form>
+      </Formik>
     </div>
-  )
+  );
 }
 
 export default Registration
