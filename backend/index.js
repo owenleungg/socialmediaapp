ee2a990f3f1193d04889e72780804076ddc9e4e7
Owen Leung <owenleung2005@gmail.com>
2025-01-03 23:37:46 -0500

    8

--- backend/index.js
+++ backend/index.js
@@ -10,9 +10,10 @@ const db = require("./models")
 // Routers
 const postRouter = require('./routes/posts');
 app.use("/posts", postRouter);
-
 const commentsRouter = require('./routes/Comments');
 app.use("/comments", commentsRouter);
+const usersRouter = require('./routes/Users');
+app.use("/auth", usersRouter);
 
 db.sequelize.sync().then(() => {
     app.listen(3001, () =>  { // anonymous function that runs whenever server starts
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import CreatePost from './pages/CreatePost';
 import Post from './pages/Post';
+import Registration from './pages/Registration';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -10,12 +12,16 @@ function App() {
       <Router>
         <div className="navbar">
         <Link to="/createpost"> Create A Post </Link>
+        <Link to="/login"> Login </Link>
+        <Link to="/registration"> Registration </Link>
         <Link to="/"> Home </Link>
         </div>
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/createpost" element={<CreatePost/>} />
           <Route path="/post/:id" element={<Post/>} />
+          <Route path="/registration" element={<Registration/>} />
+          <Route path="/login" element={<Login/>} />
         </Routes>
       </Router>
     </div>
